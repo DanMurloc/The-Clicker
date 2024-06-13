8b05bb4119ed502bd76fd4574ebb003ffed1968a
DanMurloc <strokalov1@gmail.com>
2024-06-13 16:45:53 +0300

    Просто кликать финал

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.clicker;
 
+import static java.lang.Math.random;
+
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
             hideSystemUI();
         }
     }
+    Random rand=new Random();;
     public void onClick(View view){
         ImageView clicAltushka = (ImageView) view;
         counterClic++;
@@ -60,9 +65,27 @@ public class MainActivity extends AppCompatActivity {
             TextClick.setText("STOP!!!!!");
         }
         else if (counterClic==2001L) {
-            clicAltushka.setImageResource(R.drawable.altuha6);
             TextClick.setText("OK");
         }
+        else if (counterClic>2001L){
+            int max = 6; // Максимальное число для диапазона от 0 до max
+            int min= 1; // Максимальное число для диапазона от 0 до max
+            // Генерация случайного числа в диапазоне от min до max
+            int randomNum = rand.nextInt(max - min + 1) + min;
+            if (randomNum==1)
+                clicAltushka.setImageResource(R.drawable.altuha);
+            if (randomNum==2)
+                clicAltushka.setImageResource(R.drawable.altuha2);
+            if (randomNum==3)
+                clicAltushka.setImageResource(R.drawable.altuha3);
+            if (randomNum==4)
+                clicAltushka.setImageResource(R.drawable.altuha4);
+            if (randomNum==5)
+                clicAltushka.setImageResource(R.drawable.altuha5);
+            if (randomNum==6)
+                clicAltushka.setImageResource(R.drawable.altuha6);
+        }
+
 
     }
 }
